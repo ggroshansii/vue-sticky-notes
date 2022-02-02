24237aa0c570ced898621868ab9f757c3f5a14cc
ggroshansii <garthgroshansii@gmail.com>
2022-02-02 18:30:13 -0500

    added responsive grid to notes-display-container

--- src/components/NotesDisplay.vue
+++ src/components/NotesDisplay.vue
@@ -1,17 +1,35 @@
 <template>
-    <div class="notes-display-container">
-        <li v-for="note in notes" :key="note.id">{{note.note}}</li>
-    </div>
+  <div class="notes-display-container">
+    <div v-for="note in notes" :key="note.id" class="sticky-note">{{ note.note }}</div>
+  </div>
 </template>
 
 <script>
-    export default {
-        props: {
-            notes: Array,
-        }
-    }
+export default {
+  props: {
+    notes: Array,
+  },
+};
 </script>
 
 <style scoped>
+.notes-display-container {
+  display: grid;
+  grid-template-columns: repeat(
+    auto-fill,
+    minmax(200px, 1fr));
+  grid-gap: 1rem;
+  justify-content: center;
+  justify-items: center;
+  align-content: center;
+  align-items: center;
+  margin: 3rem auto;
+}
 
+.sticky-note {
+    background-color: yellow;
+    width: 200px;
+    height: 200px;
+    padding: 30px;
+}
 </style>
