360e5f7458c895ae888422984c663e2315a3b2b6
ggroshansii <garthgroshansii@gmail.com>
2022-02-03 10:19:18 -0500

    broke down the NoteDisplay component to have individual Note components

---
+++ src/components/Note.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <div @click="deleteNote" class="sticky-note" :id="note.id">{{note.note}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    notes: Array,
+    note: Object,
+  },
+  methods: {
+    deleteNote(e) {
+      let key = e.target.id;
+      console.log("KEY",key)
+      let index = this.notes.findIndex((obj) => obj.id === key);
+      console.log("INDEX", index);
+      this.notes.splice(index, 1);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.sticky-note {
+  font-family: cursive;
+  background-color: yellow;
+  width: 200px;
+  height: 200px;
+  padding: 30px;
+}
+</style>
--- src/components/NotesDisplay.vue
+++ src/components/NotesDisplay.vue
@@ -1,36 +1,42 @@
 <template>
   <div class="notes-display-container">
-    <div @click="deleteNote" v-for="note in notes" :key="note.id" :id="note.id" class="sticky-note">{{ note.note }}</div>
+    <app-note
+      :notes="notes"
+      v-for="note in notes"
+      :note="note"
+      :key="note.id"
+      :id="note.id"
+      ></app-note
+    >
   </div>
 </template>
 
 <script>
+import Note from "./Note.vue";
+
 export default {
   props: {
     notes: Array,
   },
+  components: {
+    appNote: Note,
+  },
   methods: {
-      deleteNote(e) {
-          let key = e.target.id;
-          let index = this.notes.findIndex((obj) => obj.id === key);
-          this.notes.splice(index, 1);
-          this.$emit("changedCount", this.count);
-      },
+
   },
   computed: {
-      count: function() {
-          return this.notes.length;
-      }
-  }
+    count: function () {
+      this.$emit("changedCount", this.count);
+      return this.notes.length;
+    },
+  },
 };
 </script>
 
 <style scoped>
 .notes-display-container {
   display: grid;
-  grid-template-columns: repeat(
-    auto-fill,
-    minmax(200px, 1fr));
+  grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
   grid-gap: 1rem;
   justify-content: center;
   justify-items: center;
@@ -40,11 +46,5 @@ export default {
   padding: 0 100px;
 }
 
-.sticky-note {
-    font-family: cursive;
-    background-color: yellow;
-    width: 200px;
-    height: 200px;
-    padding: 30px;
-}
+
 </style>
